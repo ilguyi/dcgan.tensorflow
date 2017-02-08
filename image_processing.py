6d7e826bdbdff99dc2a0b2818ba48ec636f7cfc6
ilguyi <ilgu.yi.219@gmail.com>
2017-02-08 12:22:36 +0900

    dcgan

--- image_processing.py
+++ image_processing.py
@@ -288,8 +288,8 @@ def image_preprocessing(image_buffer, train, thread_id=0):
     image = eval_image(image, height, width)
 
   # Finally, rescale to [-1,1] instead of [0, 1)
-  image = tf.sub(image, 0.5)
-  image = tf.mul(image, 2.0)
+  image = tf.subtract(image, 0.5)
+  image = tf.multiply(image, 2.0)
 
   return image
 
